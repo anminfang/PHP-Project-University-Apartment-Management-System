73fa1ef046a00a62cdc51469acf122360a5815f8
Anmin Fang <36774047+anminfang@users.noreply.github.com>
2018-07-17 23:52:16 -0600

    Create review.php

---
+++ review.php
@@ -0,0 +1,80 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+require_once '../phpCommon/siteCommon.php';
+
+displayPageHeader("Reviews");
+
+?>
+
+<p> What our residents are saying about us! </p>
+        <section id="reviews">
+            <br />
+            <br />
+            <img src="../image/user1.png" style="float:left; width:60px; height:60px " />
+            <br />
+            <br />
+            <img src="../image/stars.png" style="width:70px; height:12px" />
+            <p>
+
+                <strong>
+                    Sansa Stark:  <br />
+                </strong>
+                <i>7 hours ago..</i>
+                <br />
+                <br />
+                Staff are friendly and bus stop very close! Swimming pools and tennis courts are a plus as well
+            </p>
+            <br />
+
+            <img src="../image/user2.png" style="float:left; width:60px; height:60px" />
+            <br />
+            <br />
+            <img src="../image/stars.png" style="width:70px; height:12px" />
+
+            <p>
+                <strong>
+                    Jon Snow: <br />
+                </strong>
+                <i>
+                    2 days ago..
+                </i>
+                <br />
+                <br />
+                I love the view from my room, and how quiet it is. My dog loves playing in the yard
+            </p>
+            <br />
+
+            <img src="../image/user3.png" style="float:left; width:60px; height:60px" />
+            <br />
+            <br />
+            <img src="../image/stars.png" style="width:70px; height:12px" />
+            <p>
+                <strong>
+                    Jamie Lannister: <br />
+                </strong>
+                <i>
+                    1 week ago..
+                </i>
+                <br />
+                <br />
+
+                There are no market places nearby, which makes shopping really difficult :(
+            </p>
+            <br />
+
+        </section>
+        <br />
+        <br />
+        
+        <?php
+        
+        displayPageFooter();
+        
+        ?>
